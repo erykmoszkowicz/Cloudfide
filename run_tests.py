2c574e97f93b6e9b50a2987cc7f834ffac71cda7
erykmoszkowicz <eryk.moszkowicz@gmail.com>
2025-11-25 08:56:31 +0100

    Add solution and run_tests.py

---
+++ run_tests.py
@@ -0,0 +1,46 @@
+from test_virtual_column import *
+
+all_tests = [
+    test_sum_of_two_columns,
+    test_multiplication_of_two_columns,
+    test_subtraction_of_two_columns,
+    test_empty_result_when_invalid_labels,
+    test_empty_result_when_invalid_rules,
+    test_when_extra_spaces_in_rules
+]
+
+passed_count = 0
+failed_tests = []
+
+# Iterate over all tests
+for test_func in all_tests:
+    test_name = test_func.__name__
+    # Passed tests
+    try:
+        test_func()
+        print(f"Passed: {test_name}")
+        passed_count += 1
+    
+    # Failed tests
+    except AssertionError as e:
+        print(f"Failed: {test_name}")
+        failed_tests.append((test_name, e))
+
+    except Exception as e:
+        print(f"Error: {test_name}: {type(e).__name__}")
+        failed_tests.append((test_name, e))
+
+# Number of total tests
+total_tests = len(all_tests)
+
+# Final tests results
+if passed_count == total_tests:
+    print(f"\nAll {total_tests} tests passed.")
+
+else:
+    failed_count = total_tests - passed_count
+    print(f"Failed: {failed_count}/{total_tests} tests.")
+    
+    for name, error in failed_tests:
+        print(f"\n[{name}]")
+        print(str(error).split('\n', 1)[0]) 
--- solution.py
+++ solution.py
@@ -1,12 +1,45 @@
 import pandas as pd
 import re
 
-
-
-fruits_sales = pd.DataFrame(
-    {'name': ['banana','apple'],
-     'quantity': [10,3],
-     'price': [10,1]}
-)
-
 def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataFrame:
+    
+    # Check arguments datatypes
+    if not isinstance(df, pd.DataFrame) or not isinstance(role, str) or not isinstance(new_column, str):
+        return pd.DataFrame() 
+
+    # Check 
+    if not re.fullmatch(r'^[a-zA-Z_]+$', new_column):
+        return pd.DataFrame()
+    
+    # Check role expression. Acceptable format = col_1 [* OR + OR -] col_2
+    # Delete spaces for easier parsing
+    clean_role = role.replace(" ", "")
+    if not re.fullmatch(r'([a-zA-Z_]+(?:[+\-*][a-zA-Z_]+)+)', clean_role):
+        return pd.DataFrame()
+
+    # Get all potential columns from role expression
+    cols = re.findall(r'[a-zA-Z_]+', clean_role)
+
+    # Check if all potential columns are in original DataFrame
+    for col in cols:
+        if col not in df.columns:
+            return pd.DataFrame()
+    
+    # Return empty DataFrame if we want to add column that already exist.
+    if new_column in df.columns:
+        return pd.DataFrame()
+    
+    try:
+        # Keep original DataFrame as is
+        result_df = df.copy()
+        
+        # Expression for pd.eval func
+        expression = f"{new_column} = {role}"
+        
+        # Apply eval
+        result_df = result_df.eval(expression)
+        
+        return result_df
+        
+    except Exception:
+        return pd.DataFrame()
